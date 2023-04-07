c21e938bf56922f646aa74091bd2ce37de60ebf4
jay bhogayata <jaybhogayata53@gmail.com>
2023-04-07 08:38:03 +0530

    add user router and controllers

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./config/db");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 // Initializing connection to DB
 connectToDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use("/", userRouter);
 
 module.exports = app;
